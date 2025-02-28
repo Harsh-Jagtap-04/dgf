3ceb36e911f63e97def37ee33f5445b333f0d730
Harsh Jagtap <97657785+Harsh-Jagtap-04@users.noreply.github.com>
2025-02-28 08:13:52 +0530

    Add files via upload

---
+++ DGF_Backend/routes/employeeSearchByNameRoutes.js
@@ -0,0 +1,9 @@
+//routes/employeeSearchByNameRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const { searchEmployees } = require('../controllers/employeeSearchByNameController');
+
+router.get('/searchEmployeesByName', searchEmployees);
+
+module.exports = router;
